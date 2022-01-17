51934b24e1133ef0cd0d1043c50723d84da261cd
Smuia <simon.muia@positrust.com>
2022-01-17 16:52:39 +0300

    add build folder for production mode

---
+++ client/build/precache-manifest.55ddcf6439bcbe5825000e700eaaac8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1e961ef89e087605e9006c1c53cdbb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44124d3c48bc4d7453e6",
+    "url": "/static/css/2.09699a42.chunk.css"
+  },
+  {
+    "revision": "da84d1ac63c1e20108be",
+    "url": "/static/css/main.21f7efc9.chunk.css"
+  },
+  {
+    "revision": "44124d3c48bc4d7453e6",
+    "url": "/static/js/2.eb0ec2c7.chunk.js"
+  },
+  {
+    "revision": "da84d1ac63c1e20108be",
+    "url": "/static/js/main.f66d2568.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
