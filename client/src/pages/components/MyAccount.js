997855b15cac856264daccbc581f114432e4ff9a
Smuia <simon.muia@positrust.com>
2022-01-18 02:58:03 +0300

    publish to heroku commit

--- client/src/pages/components/MyAccount.js
+++ client/src/pages/components/MyAccount.js
@@ -2,19 +2,28 @@ import React from 'react';
 import {Container,Row, Col, Button, Table} from 'react-bootstrap';
 import './css/MyAccount.css';
 import {PersonBoundingBox,DoorOpenFill} from 'react-bootstrap-icons';
+import { useHistory } from 'react-router-dom';
+import useWeb3React from 'web3-react/core-js';
 
 
 
-
+//define logout function
+const onLogOut = (deactivate, cb) => {
+    deactivate();
+    cb();
+  };
 
 export default  function MyAccountComponent(){
+
+    const history = useHistory();
+    const {deactivate} = useWeb3React();
 
   
     return(
         
         <Container className="Profile-wrapper">
             <Row>
-                <Col><Button variant="outline-warning"> <DoorOpenFill/> Logout</Button></Col>
+                <Col><Button variant="outline-warning"  onClick={() => onLogOut(deactivate, () => history.push('/'))}> <DoorOpenFill/> Logout</Button></Col>
             </Row>
             
             <Row>
